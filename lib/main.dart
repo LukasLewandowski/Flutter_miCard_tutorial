00b76da2ec773b333bbcc47cbf72e94e9362e905
LukasLewandowski <llewandowski1990@gmail.com>
2021-04-25 23:02:02 +0200

    add my photo and name

--- lib/main.dart
+++ lib/main.dart
@@ -10,56 +10,78 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Stack(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  width: 100.0,
-                  // margin: EdgeInsets.only(left: 30),
-                  // padding: EdgeInsets.all(20),
-                  color: Colors.red,
-                  child: Text('Container 1'),
-                ),
-                Container(
-                  width: 100.0,
-                  color: Colors.blue,
-                  child: Text('Container 2'),
-                ),
-              ],
-            ),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Center(
+              child: Column(
                 children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                    child: Text('Container 3'),
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/lukas.jpg'),
                   ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.greenAccent,
-                    child: Text('Container 4'),
+                  Text(
+                    'Lukas Lewandowski',
+                    style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
                   ),
-                  Container(
-                    width: 250.0,
-                  ),
-                ]),
-            // Image(
-            //   image: AssetImage('images/diamond.png'),
-            //   // image: NetworkImage(
-            //   //     'https://images.pexels.com/photos/2387876/pexels-photo-2387876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
-            // )
-          ],
-        )),
-      ),
+                  Text(
+                    'Flutter Junior Dev',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade100,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            //     child: Stack(
+            //   children: <Widget>[
+            //     Row(
+            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //       crossAxisAlignment: CrossAxisAlignment.stretch,
+            //       children: <Widget>[
+            //         Container(
+            //           width: 100.0,
+            //           // margin: EdgeInsets.only(left: 30),
+            //           // padding: EdgeInsets.all(20),
+            //           color: Colors.red,
+            //           child: Text('Container 1'),
+            //         ),
+            //         Container(
+            //           width: 100.0,
+            //           color: Colors.blue,
+            //           child: Text('Container 2'),
+            //         ),
+            //       ],
+            //     ),
+            //     Column(
+            //         mainAxisAlignment: MainAxisAlignment.center,
+            //         crossAxisAlignment: CrossAxisAlignment.end,
+            //         children: <Widget>[
+            //           Container(
+            //             height: 100.0,
+            //             width: 100.0,
+            //             color: Colors.yellow,
+            //             child: Text('Container 3'),
+            //           ),
+            //           Container(
+            //             height: 100.0,
+            //             width: 100.0,
+            //             color: Colors.greenAccent,
+            //             child: Text('Container 4'),
+            //           ),
+            //           Container(
+            //             width: 250.0,
+            //           ),
+            //         ]),
+            //   ],
+            // )),
+          )),
     );
   }
 }
